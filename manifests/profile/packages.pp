6b40bc21c3caee20a703fffa6e2fef135091f836
Maik Zumstrull <m.zumstrull@syseleven.de>
2015-05-15 13:40:28 +0200

    os-603 Purge avahi-daemon and cloud-init on hardware

--- manifests/profile/packages.pp
+++ manifests/profile/packages.pp
@@ -1,16 +1,38 @@
 # Installs various packages.
 class base::profile::packages (
 ) {
-  ensure_packages( [
-                    'acl',
-                    'attr',
-                    'bash-completion',
-                    'git',
-                    'ipmitool',
-                    'moreutils',
-                    'ntp',
-                    'sysstat',
-                    'wget',
-                    'whois',
-                  ])
+  $install_everywhere = [
+    'acl',
+    'attr',
+    'bash-completion',
+    'git',
+    'ipmitool',
+    'moreutils',
+    'ntp',
+    'sysstat',
+    'wget',
+    'whois',
+  ]
+
+  $purge_everywhere = [
+    'eatmydata',
+  ]
+
+  $not_on_metal = [
+    'avahi-daemon',
+    'cloud-init',
+  ]
+
+  ensure_packages($install_everywhere)
+
+  package { $purge_everywhere:
+    ensure => purged,
+  }
+
+  if $::is_virtual == 'false' {
+    package { $not_on_metal:
+      ensure => purged,
+    }
+  }
+
 }
