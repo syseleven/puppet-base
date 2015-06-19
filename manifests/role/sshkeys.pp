d958041bcb20dd26d55b61332e5169b8eb18674e
Johannes Grassler <j.grassler@syseleven.de>
2015-06-19 17:53:52 +0200

    os-599 Added default namespace for role::sshkeys and improved ssh::key_sources splitting.

--- manifests/role/sshkeys.pp
+++ manifests/role/sshkeys.pp
@@ -1,9 +1,10 @@
 # Deploys SSH authorized keys from an arbitrary number of ssh::authorized_keys
 # style hashes (either in global (default) or the ssh::keys:: name space).
 class base::role::sshkeys (
-  $key_sources = hiera('ssh::key_sources', split($::sshkeys, ' ')),
-  $namespace = hiera('ssh::key_namespace', undef),
+  $key_sources = hiera('ssh::key_sources', split($::sshkeys, '\s+')),
+  $namespace = hiera('ssh::key_namespace', 'ssh::keys'),
 ) {
+
     $keys_merged = merge_hiera_hashes($key_sources, $namespace)
 
     class { 'base::profile::ssh::authorized_keys':
