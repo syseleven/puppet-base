f24868bbd11714de7f256e4cb4a121bf9c1cd271
Johannes Grassler <j.grassler@syseleven.de>
2015-06-25 10:49:58 +0200

    os-599 Overhauled SSH key source lookup
    * Removed ssh::key_namespace (source of confusion).
    * Added sane default for both empty ssh::key_sources and empty sshkeys metadata parameter.
    * Hardwired ssh::authorized_keys_authorized keys into ssh::key_sources to
      accomodate legacy setups.

--- manifests/role/sshkeys.pp
+++ manifests/role/sshkeys.pp
@@ -2,10 +2,15 @@
 # style hashes (either in global (default) or the ssh::keys:: name space).
 class base::role::sshkeys (
   $key_sources = hiera('ssh::key_sources', split($::sshkeys, '\s+')),
-  $namespace = hiera('ssh::key_namespace', 'ssh::keys'),
 ) {
 
-    $keys_merged = merge_hiera_hashes($key_sources, $namespace)
+    if $key_sources == '' {
+      $_key_sources = concat([ 'ssh::keys::sys11', 'ssh::keys::user' ], ['ssh::authorized_keys::authorized_keys'])
+      } else {
+      $_key_sources = concat($key_sources, ['ssh::authorized_keys::authorized_keys'])
+    }
+
+    $keys_merged = merge_hiera_hashes($_key_sources)
 
     class { 'base::profile::ssh::authorized_keys':
       authorized_keys => $keys_merged,
