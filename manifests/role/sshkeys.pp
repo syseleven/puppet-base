02d56649a19c924a72e2e0c1fb7cc40c9e964b22
Johannes Grassler <j.grassler@syseleven.de>
2015-06-25 11:57:28 +0200

    os-599 Added existence check for ssh::authorized_keys::authorized_keys to prevent unneccessary warnings from merge_hiera_hashes().

--- manifests/role/sshkeys.pp
+++ manifests/role/sshkeys.pp
@@ -4,10 +4,23 @@ class base::role::sshkeys (
   $key_sources = hiera('ssh::key_sources', split($::sshkeys, '\s+')),
 ) {
 
-    if $key_sources == [] {
-      $_key_sources = concat([ 'ssh::keys::sys11', 'ssh::keys::user' ], ['ssh::authorized_keys::authorized_keys'])
+    # Accomodate project-config repositories that still use the old mechanism's
+    # ssh::authorized_keys::authorized_keys variable. This will only be added
+    # to the list of key sources if it actually exists.
+    $keys_legacy = hiera('ssh::authorized_keys::authorized_keys', {})
+
+    if empty($key_sources) {
+      if ! empty($keys_legacy) {
+        $_key_sources = concat([ 'ssh::keys::sys11', 'ssh::keys::user' ], ['ssh::authorized_keys::authorized_keys'])
+        } else {
+          $_key_sources = [ 'ssh::keys::sys11', 'ssh::keys::user' ]
+        }
       } else {
-      $_key_sources = concat($key_sources, ['ssh::authorized_keys::authorized_keys'])
+        if ! empty($keys_legacy) {
+          $_key_sources = concat($key_sources, ['ssh::authorized_keys::authorized_keys'])
+        } else {
+          $_key_sources = $key_sources
+        }
     }
 
 
